9235d2a8729c91c5a6c8ee439920bcb31f5aba75
Inés María O <inesmao@misena.edu.co>
2020-09-25 21:53:18 -0500

    Se actualizó el index.php

--- views/index.php
+++ views/index.php
@@ -1,12 +1,16 @@
 <?php require("partials/routes.php"); ?>
 <?php require("partials/check_login.php"); ?>
 
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <title><?= getenv('TITLE_SITE') ?> | Home</title>
-    <?php require("partials/head_imports.php"); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-
+    <?php echo "Hola Cliente" ?>
 </body>
+</html>
